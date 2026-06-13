3a2c506f9ab43c66cb12017ff3ee352a327dd302
Ranjan Sharma <73110351+iranjansharma@users.noreply.github.com>
2026-06-13 20:13:09 +0530

    Final

--- components/contact-02/contactSection.tsx
+++ components/contact-02/contactSection.tsx
@@ -85,7 +85,8 @@ export default function ContactSection(): React.ReactElement {
           I Mr. Vikas Sharma  Assistant General Manager (AGM)  <br />
           <Link className="font-medium text-[#febd59]" href="tel:+919560748636">
             +91 9560-748-636
-          </Link></br>
+          </Link>
+          <br/>
           <Link className="font-medium text-[#febd59]" href="mailto:sgtbsairoparts@gmail.com">
             sgtbsairoparts@gmail.com
           </Link>
